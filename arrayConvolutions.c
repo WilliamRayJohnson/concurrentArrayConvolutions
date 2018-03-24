a64daae29bdee6761cfd0e05c3f75c26cd4078f6
William Johnson <willrayjohnson@gmail.com>
2018-03-24 00:27:30 -0500

    Added ops files and code for setup of threads

--- arrayConvolutions.c
+++ arrayConvolutions.c
@@ -11,12 +11,54 @@
 #define THREAD_CREATION_FAILED -1
 #define THREAD_JOIN_FAILED -2
 
+typedef struct atomic_i {
+    int value;
+    pthread_mutex_t *lock;
+} Atomic_Int;
+
+typedef struct thread_input {
+    Atomic_Int *convArrayP;
+    char *opsFile;
+} Conv_Input;
+
+void *perform_ops(void *arg);
+
 int main(int argc, char *argv[]) {
     if(argc>=3) {
-        printf("You provided %d arguments\n", argc);
+        int i;
+
+        /* Create the shared memory array, initialize its int value
+         * and lock */
+        int convArraySize;
+        sscanf(argv[1], "%d", &convArraySize);
+        Atomic_Int *convArray[convArraySize];
+        for (i = 0; i < convArraySize; i++) {
+            convArray[i] = (Atomic_Int *) malloc(sizeof(Atomic_Int));
+            convArray[i]->value = 0;
+            convArray[i]->lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+            pthread_mutex_init(convArray[i]->lock, NULL);
+        }
+
+        /* Initialize threads and run them */
+        int threadCount = argc - 2;
+        pthread_t *tid[threadCount];
+        for (i = 0; i < threadCount; i++) {
+            Conv_Input *thdArg = (Conv_Input *) malloc(sizeof(Conv_Input));
+            thdArg->convArrayP = convArray;
+            thdArg->opsFile = argv[i + 2];
+        }
+
     }
     else
         printf("Please provide the appropriate number of command line arguments\n");
 
     exit(0);
+}
+
+/**
+ * Reads in a file and performs the operations listed
+ * within it on a shared array
+ */
+void *perform_ops(void *arg) {
+    pthread_exit(NULL);
 }
